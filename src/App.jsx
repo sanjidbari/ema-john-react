93cb5b83378c2ff9557df6e7476106ed0687a509
Sanjid Bari <sanjidbari@sanjids-air.home>
2023-04-30 11:40:33 -0400

    product cart added

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import './App.css'
 import Header from './component/Header/Header'
+import Shop from './component/Shop/Shop'
 
 function App() {
   return (
-    <>
+    <div>
       <Header></Header>
-    </>
+      <Shop></Shop>
+    </div>
   )
 }
 
---
+++ src/component/Shop/Shop.jsx
@@ -0,0 +1,38 @@
+import React, { useEffect, useState } from 'react';
+import './Shop.css'
+import Product from '../Product/Product';
+
+const Shop = () => {
+    const[products, setProducts] = useState([]);
+    const[cart, setCart] = useState([]);
+
+    useEffect(() => {
+        fetch('../../../public/products.json')
+        .then(res => res.json())
+        .then(data => setProducts(data))
+    }, []);
+
+    const handleAddToCart = (product) => {
+        const newCart = [...cart, product]
+        setCart(newCart);
+    }
+
+    return (
+        <div className='shop-container'>
+            <div className='products-container'>
+                {
+                    products.map(product => <Product 
+                        key={product.id} 
+                        product={product}
+                        handleAddToCart={handleAddToCart}>
+                        </Product>)
+                }
+            </div>
+            <div className='cart-container'>
+                <p>Order summary: {cart.length}</p>
+            </div>
+        </div>
+    );
+};
+
+export default Shop;
